e1bd7c2778c42ed2b2326b0ec013ee3537384d20
valeraGorobets <valera.gorobets@gmail.com>
2016-08-23 16:36:28 +0300

    class queue is cleaned up

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,35 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
 
+	constructor(maxSize) {
+		this.maxSize = 30;
+		if(maxSize != null){
+			this.maxSize = maxSize;
+		}
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.heap.size() == this.maxSize){
+			throw "queue has max size"; 
+		}
+		this.heap.push(data,priority);
 	}
 
 	shift() {
-
+		if(this.isEmpty()){
+			throw "queue is empty"; 
+		}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
-
+	
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
