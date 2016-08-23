f9cceae5245224b9c070cd4308a07998bae91910
valeraGorobets <valera.gorobets@gmail.com>
2016-08-23 16:32:44 +0300

    class node is cleaned up

--- src/node.js
+++ src/node.js
@@ -1,37 +1,37 @@
 class Node {
+
 	constructor(data, priority) {
-		this.data=data;
-		this.priority=priority;
-		this.parent=null;
-		this.left=null;
-		this.right=null;
+	   this.data = data;
+	   this.priority = priority;
+	   this.parent = null;
+	   this.left = null;
+	   this.right = null;
 	}
 
-	appendChild(node) {
-		if(this.left==null){
-			this.left=node;
-		}else if(this.right==null){
-			this.right=node;
-		}
-		node.parent=this;
+    appendChild(node) {
+	   if(this.left == null){
+		  this.left = node;
+	   } else if(this.right == null){
+		  this.right = node;
+	   }
+       node.parent=this;
 	}
 
-	removeChild(node) {
-		node.parent=null;
-		if(this.left==node){
-			this.left=null;
-		}else if(this.right==node){
-			this.right=null;
-		}
-		else{
-			throw "node is not a child of this node"; 
+    removeChild(node) {
+	   node.parent = null;
+	   if(this.left == node){
+	       this.left = null;
+		} else if(this.right == node){
+            this.right = null;
+        } else{
+		  throw "node is not a child of this node"; 
 		}
 
 	}
 
 	remove() {
-		if(this.parent==null){
-			return;
+		if(this.parent == null){
+		  return;
 		}
 		this.parent.removeChild(this);
 	}
@@ -44,46 +44,50 @@ class Node {
         let copyOfParentOfParent = this.parent.parent;
         let copyOfParent = this.parent;
         let copyOfThis = this;
-        let thisLeftSon=this.left;
-        let thisRightSon=this.right;
-        let parentOtherSon;
-        let isThisLeftFromParent = this.parent.left==this ? true : false;
-     
-        if(isThisLeftFromParent){
-    	    parentOtherSon=this.parent.right;
+        let thisLeftChild = this.left;
+        let thisRightChild = this.right;
+        let isThisParentsLeftChild = this.parent.left==this ? true : false;
+        let parentsSecondChild;
+        if(isThisParentsLeftChild){
+            parentsSecondChild = this.parent.right;
     	}else{
-    		parentOtherSon=this.parent.left;
+            parentsSecondChild = this.parent.left;
     	}
-    	  
-    	//
-    
+
+    	//remove This and its parent
     	this.parent.remove();
     	this.remove();
-    	//
-    	if(copyOfParentOfParent!=null){
+
+    	//merge PoP with This, merge This with Parents second child
+    	if(copyOfParentOfParent != null){
             copyOfParentOfParent.appendChild(copyOfThis);
     	}
-    	if(parentOtherSon!=null){
-    	    
-            if(isThisLeftFromParent){
-        	    copyOfThis.right=parentOtherSon;
-        	    copyOfThis.left=null;
+    	if(parentsSecondChild != null){
+            if(isThisParentsLeftChild){
+                copyOfThis.right = parentsSecondChild;
+        	    copyOfThis.left = null;
             }else{
-        	    copyOfThis.left=parentOtherSon;
-        	    copyOfThis.right=null;
+                copyOfThis.left = parentsSecondChild;
+                copyOfThis.right = null;
         	}
-        	parentOtherSon.parent=copyOfThis;
+        	parentsSecondChild.parent = copyOfThis;
         }
-        //
+        
+        //merge Parent with This childs
         copyOfThis.appendChild(copyOfParent);
-    	if(thisLeftSon!=null){
-            copyOfParent.left=thisLeftSon;
-        	thisLeftSon.parent=copyOfParent;
+    	if(thisLeftChild != null){
+            copyOfParent.left = thisLeftChild;
+            thisLeftChild.parent = copyOfParent;
+        }else{
+            copyOfParent.left = null;
         }
-        if(thisRightSon!=null){
-        	copyOfParent.right=thisRightSon;
-        	thisRightSon.parent=copyOfParent;
-        }	
+        if(thisRightChild != null){
+            copyOfParent.right = thisRightChild;
+        	thisRightChild.parent = copyOfParent;
+        }
+        else{
+            copyOfParent.right = null;
+        }
 	}
 }
 
